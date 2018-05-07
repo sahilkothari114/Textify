cdd7551c840b3add2ae7b313825873cc1898ffe6
sahil <sahilkothari114@gmail.com>
2018-05-08 02:39:46 +0530

    File Explorer Feature

--- src/application/IndexController.java
+++ src/application/IndexController.java
@@ -1,5 +1,43 @@
 package application;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Collection;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class IndexController {
-
+	@FXML
+	private TextField imagePath;
+	
+	@FXML
+	private ImageView ImageChooser;
+	
+	@FXML
+	private ImageView selectedImageView;
+	
+	@FXML
+	public void chooseImage(MouseEvent e) {
+		FileChooser fc = new FileChooser();
+		fc.getExtensionFilters().addAll(new ExtensionFilter("JPG files","*.jpg"));
+		File selectedFile = fc.showOpenDialog(null);
+		imagePath.setText(selectedFile.getAbsolutePath());
+	
+		FileInputStream inputstream = null;
+		try {
+			inputstream = new FileInputStream(selectedFile.getAbsolutePath());
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+		Image image = new Image(inputstream);
+		selectedImageView.setImage(image);
+	}
+	
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -7,19 +7,34 @@ import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent; 
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Index.fxml"));
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			IndexController ic = new IndexController();
+			//fxmlLoader.setController(ic);
+			fxmlLoader.setLocation(getClass().getResource("Index.fxml"));
+			Parent root = fxmlLoader.load();
 			primaryStage.initStyle(StageStyle.TRANSPARENT);
 			Scene scene = new Scene(root);
 			scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+		      //Creating the mouse event handler 
+		      EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() { 
+		         @Override 
+		         public void handle(MouseEvent e) { 
+		            ic.chooseImage(e);
+		           
+		         } 
+		      };
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
